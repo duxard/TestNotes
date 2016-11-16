d390c5defa4ca2ef1d326305d18c892098624dfb
duxard <duxard@gmail.com>
2016-11-16 17:32:19 +0300

    third

--- note-creating/note-creating.component.js
+++ note-creating/note-creating.component.js
@@ -3,9 +3,34 @@
 angular.module('noteCreating').component('noteCreating', {
     templateUrl: 'note-creating/note-creating.template.html',
     controller: ['$scope', '$window', function($scope, $window){
-                $scope.saveNote = function(){
+                var $btnSave = $('.btn-save'),
+                    $btnAdd =  $('.btn-add'),
+                    $btnClose = $('.btn-close'),
+                    newOne = "true";
+                            
+                $scope.saveNote = function(newOne){
                     localStorage.setItem(this.noteHeader, this.noteText);
+      /****************************************************            
+            var header = "one";
+            var text = "lorem ipsum dlorum";
+            var tags = ['one','two','three'];
+            var myObj = {msg: text, hash: tags};
+            
+            var sObj = JSON.stringify(myObj);
+            localStorage.setItem(header , sObj);
+            
+     /****************************************************/          
+                    
                     console.log(localStorage);
+                    if(!arguments.length){
+                        $window.location.href = '/';
+                    }else{
+                        $scope.noteHeader = '';
+                        $scope.noteText = '';
+                    }
+                }
+                
+                $scope.close = function(){
                     $window.location.href = '/';
                 }
             }
